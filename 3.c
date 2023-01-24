0b7b189d8ad6790bce7ce9cf60a0431d3fd7a255
hemanth-sai31 <122268796+hemanth-sai31@users.noreply.github.com>
2023-01-23 22:46:48 -0800

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+int x,fact=1,n;
+printf("enter N value : ");
+scanf("%d",&n);
+for(x=1;x<=n;x++)
+	fact=fact*x;
+printf("Factorical of %d is %d",n,fact);
+return 0;
+}
