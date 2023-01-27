faaff324245bc1cf7c0c7b71ca341375d6f4f0d5
hemanth-sai31 <122268796+hemanth-sai31@users.noreply.github.com>
2023-01-27 01:12:10 -0800

    Update 32.c

--- 32.c
+++ 32.c
@@ -1,30 +1,23 @@
-#include <stdio.h>
-
-
-
-int main() 
-
-{
-    int i,n;
-	int arr[n];
-
-    
-   
-    printf("Enter the size of the array: ");
-    scanf("%d", &n);
-
-  
-    printf("Enter an array: ");
-    for(i=0;i<n;i++)
-	{
-        scanf("%d", &arr[i]);
-    }
-
-    printf("Reversed array: ");
-    for (i=n-1;i>=0;i--)
-	{
-        printf("%d ", arr[i]);
-    }
-
-    return 0;
-}
+#include<stdio.h>  
+int main()  
+{  
+    int n, arr[n], i;  
+    printf("Enter the size of the array: ");  
+    scanf("%d", &n);  
+    printf("Enter the elements: ");  
+    for(i = 0; i < n; i++)  
+    {  
+        scanf("%d", &arr[i]);  
+    }  
+    int rev[n], j = 0;  
+    for(i = n-1; i >= 0; i--)  
+    {  
+        rev[j] = arr[i];  
+        j++;  
+    }  
+    printf("The Reversed array: ");  
+    for(i = 0; i < n; i++)  
+    {  
+        printf("%d ", rev[i]);  
+    }  
+}  
